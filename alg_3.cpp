2a85c5f85cd36e65f9fec9934e4121b1f37a24c7
ElsoN <vhile@sina.com>
2014-09-02 17:24:53 +0800

    modfied alg_3

--- alg_3.cpp
+++ alg_3.cpp
@@ -2,6 +2,19 @@
 //NO.003
 /* 3.求子数组的最大和  */
 // 1, -2, 3, 10, -4, 7, 2, -5
+cur = 0
+sum = 1
+max = 1
+
+cur = 1
+sum = -1;
+max = 1;
+sum = 0;
+
+cur = 2;
+sum = 3;
+max = 3;
+
 
 int maxSubarray (int a[], int size)
 {
@@ -21,4 +34,47 @@ int maxSubarray (int a[], int size)
 		}
 	}
 	return max;
+}
+
+
+int maxSubarray(int a[], int size)
+{
+	if(size <= 0) error("fuck error");
+	int sum = 0;
+	int max = -(1<<31);
+	int cur = 0;
+	while( cur < size)
+	{
+		sum += a[cur++];
+		if(sum > max)
+		{
+			max = sum;
+			}
+		else if(sum < 0)
+		{
+			sum = 0;
+		}
+	}
+	return max;
+}
+
+int maxSubarray(int a[], int size)
+{
+	if(size <= 0) error("fuck error");
+	int sum = 0;
+	int max = -(1<< 31);
+	int cur = 0;
+	while(cur<size)
+	{
+		sum += a[cur++];
+		if(sum > max)
+		{
+			max = sum;
+		}
+		else if(sum < 0)
+		{
+			sum = 0;
+		}
+	}
+	return max
 }
